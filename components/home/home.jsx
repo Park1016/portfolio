93fd8728aaa096d621d58ca8ac6c51442b355d9e
hyunjeongPark <GuoHJP@gmail.com>
2021-10-13 07:24:58 +0900

    맨위로, 맨아래로 버튼 생성

--- components/home/home.jsx
+++ components/home/home.jsx
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic';
 import { faArrowCircleUp, faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
+import * as S from '../../styles/home.style';
 // import P from '../../styles/Home.module.css';
 
 const AboutMe = dynamic(() => import('../aboutMe/aboutMe'));
@@ -21,11 +22,21 @@ export default class Home extends Component {
     constructor(props){
         super(props);
         this.slide = this.slide.bind(this);
+        this.box = React.createRef();
         this.onScroll = this.onScroll.bind(this);
         this.scrollUp = this.scrollUp.bind(this);
         this.scrollDown = this.scrollDown.bind(this);
+        this.upDown = this.upDown.bind(this);
+        this.upEnter = this.upEnter.bind(this);
+        this.upLeave = this.upLeave.bind(this);
+        this.downEnter = this.downEnter.bind(this);
+        this.downLeave = this.downLeave.bind(this);
+
         this.state = {
             scroll: false,
+            slideIndex: 0,
+            upText: false,
+            downText: false
         }
     }
     slide(y){
@@ -51,6 +62,7 @@ export default class Home extends Component {
         if(check){
             this.setState({ scroll: true });
         }
+        
     }
 
     componentDidUpdate(){
@@ -73,6 +85,26 @@ export default class Home extends Component {
         this.slider.slickGoTo(3);
     }
 
+    upDown = () => {
+        console.log(this.box.current.scrollTop);
+    }
+
+    upEnter = () => {
+        this.setState({ upText: true }); 
+    }
+
+    upLeave = () => {
+        this.setState({ upText: false }); 
+    }
+
+    downEnter = () => {
+        this.setState({ downText: true }); 
+    }
+
+    downLeave = () => {
+        this.setState({ downText: false }); 
+    }
+
     render() {
 
         const text = ['aboutMe', 'skill', 'project', 'contact'];
@@ -91,13 +123,22 @@ export default class Home extends Component {
                     <p>{text[i]}</p>
                 );
             },
+            beforeChange: (current, next) => this.setState({ slideIndex: next })
         };
 
 
         return (
-            <div>
-                <FontAwesomeIcon icon={faArrowCircleUp} onClick={this.scrollUp} />
-                <FontAwesomeIcon icon={faArrowCircleDown} onClick={this.scrollDown} />
+            <div ref={this.box}>
+                <S.up onClick={this.scrollUp} up={this.state.slideIndex} onMouseEnter={this.upEnter} onMouseLeave={this.upLeave}>
+                    {this.state.upText && <p>맨위로</p>}
+                    {/* <p>맨위로</p> */}
+                </S.up>
+                <FontAwesomeIcon icon={faArrowCircleUp} />
+                <S.down onClick={this.scrollDown} down={this.state.slideIndex} onMouseEnter={this.downEnter} onMouseLeave={this.downLeave}>
+                    {this.state.downText && <p>맨아래로</p>}
+                    {/* <p>맨아래로</p> */}
+                </S.down>
+                <FontAwesomeIcon icon={faArrowCircleDown} />
                 <Slider {...settings} ref={slider => this.slider = slider}>
                     <div>
                         <AboutMe />
---
+++ styles/home.style.js
@@ -0,0 +1,79 @@
+﻿import styled, { css } from 'styled-components';
+
+const up = styled.div`
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    position: absolute;
+    bottom: 4rem;
+    /* left: calc(50vw - 1rem); */
+    right: 1rem;
+    z-index: 13;
+    p {
+        width: 4rem;
+        padding: 0.2rem 0.5rem;
+        background: rgb(0,0,0,0.5);
+        color: #fff;
+        border-radius: 10px;
+        transform: translate(-4.5rem, 0.1rem);
+    }
+    ${(props) => {
+        if (props.up === 0) {
+            return css`
+                background: rgb(255,255,255,0.9);
+                :hover {
+                    cursor: not-allowed;
+                }
+            `;
+        } else {
+            return css`
+                background: transparent;
+                :hover {
+                    cursor: pointer;
+                }
+            `;
+        }
+    }}
+`;
+
+const down = styled.div`
+    width: 2rem;
+    height: 2rem;
+    border-radius: 50%;
+    background: transparent;
+    position: absolute;
+    bottom: 2rem;
+    /* left: calc(50vw - 1rem); */
+    right: 1rem;
+    z-index: 13;
+    p {
+        width: 5rem;
+        padding: 0.2rem 0.5rem;
+        background: rgb(0,0,0,0.5);
+        color: #fff;
+        border-radius: 10px;
+        transform: translate(-5.5rem, 0.1rem);
+    }
+    ${(props) => {
+        if (props.down === 3) {
+            return css`
+                background: rgb(255,255,255,0.9);
+                :hover {
+                    cursor: not-allowed;
+                }
+            `;
+        } else {
+            return css`
+                background: transparent;
+                :hover {
+                    cursor: pointer;
+                }
+            `;
+        }
+    }}
+`;
+
+export {
+    up,
+    down,
+}
